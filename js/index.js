85507daeca17d2a68838a766527e4896b54117a6
twinzies <kjaintara@gmail.com>
2025-07-02 19:37:19 +0100

    Add blog posts section and update portfolio content
    - Add new posts.yml data file for blog functionality
    - Update publications.yml with latest research
    - Enhance styling and layout in index.scss
    - Update main page content and structure in index.html
    - Add JavaScript functionality for posts in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function() {
+    // Publication hover effects
     $('.publication-mousecell').mouseover(function() {
         $(this).find('video').css('display', 'inline-block');
         $(this).find('img').css('display', 'none');
@@ -7,5 +8,17 @@ $(document).ready(function() {
     $('.publication-mousecell').mouseout(function() {
         $(this).find('video').css('display', 'none');
         $(this).find('img').css('display', 'inline-block');
+    });
+
+    // Simple mobile navbar toggle
+    $('.navbar-burger').click(function() {
+        $('.navbar-burger').toggleClass('is-active');
+        $('.navbar-menu').toggleClass('is-active');
+    });
+
+    // Close mobile menu when clicking on a link
+    $('.navbar-item').click(function() {
+        $('.navbar-burger').removeClass('is-active');
+        $('.navbar-menu').removeClass('is-active');
     });
 })
